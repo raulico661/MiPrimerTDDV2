33d636a6883b938f13caf71cbec80473ab419122
Raúl <raulelche2001@gmail.com>
2023-03-30 16:02:34 +0200

    Añadido assert al test y atributo velocidad a Coche

--- src/Test.java
+++ src/Test.java
@@ -1,10 +1,11 @@
-import static org.junit.Assert.*;
+import org.junit.Assert;
 
 public class Test {
 	
 	@org.junit.Test
 	public void test_crear_coche() {
 		Coche nuevoCoche = new Coche();
+		Assert.assertEquals(0, nuevoCoche.velocidad);
 	}
 
 }
